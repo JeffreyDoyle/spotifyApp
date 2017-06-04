f8faa986cf92ffed82d9748674e130e242a85d48
Jeff <thejeffreydoyle@gmail.com>
2017-06-04 16:26:11 -0700

    Initial commit

---
+++ spotify-app/src/Containers/main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    This is a test.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
